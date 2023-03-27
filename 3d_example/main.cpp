6c7aeff793a691b71cdd9a0123e28e3ae184ad6e
Matteo Giuntoni <59317264+Matteo181202@users.noreply.github.com>
2023-03-27 18:22:06 +0200

    Adding camer

--- 3d_example/main.cpp
+++ 3d_example/main.cpp
@@ -7,6 +7,19 @@
 
 #include "../framework/Cube.h"
 #include "../framework/debugging.h"
+#include "../framework/Camera.h"
+
+Camera *camera = new Camera(800, 600, 45);
+
+void MouseCallBackWrapper(GLFWwindow *window, double xpos, double ypos){
+    if (camera)
+        return camera->MouseCallBack(window, xpos, ypos);
+}
+
+void ScrollCallBackWrapper(GLFWwindow *window, double xoffset, double yoffset){
+    if (camera)
+        return camera->ScrollCallBack(window, xoffset, yoffset);
+}
 
 int main(void){
     if(!glfwInit()) return -1;
@@ -28,6 +41,8 @@ int main(void){
     glewInit();
     printout_opengl_glsl_info();
 
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
     // std::vector<GLfloat> colors = {
     //     1.0f, 0.0f, 0.0f,
     //     0.0f, 1.0f, 0.0f,
@@ -43,16 +58,25 @@ int main(void){
     glEnable(GL_DEPTH_TEST);
 
     while (!glfwWindowShouldClose(window)){
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+            glfwSetWindowShouldClose(window, true);
+
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        cube->Draw();
-        glm::mat4 model = glm::rotate(glm::mat4(1.0f), (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
-        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -10.0f));
-        glm::mat4 projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+        camera->UpdateDeltaTime();
+        camera->ProcessInput(window);
+        glfwSetCursorPosCallback(window, MouseCallBackWrapper);
+        glfwSetScrollCallback(window, ScrollCallBackWrapper);
+
+        glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -5.0f));
+        glm::mat4 projection = glm::perspective(glm::radians(camera->GetFov()), 800.0f / 600.0f, 0.1f, 100.0f);
+        glm::mat4 view = camera->GetViewMatrix(window);
 
         cube->setShaderUniform<glm::mat4>(model, "model");
         cube->setShaderUniform<glm::mat4>(view, "view");
         cube->setShaderUniform<glm::mat4>(projection, "projection");
+
+        cube->Draw();
 
         glfwSwapBuffers(window);
         glfwPollEvents();
