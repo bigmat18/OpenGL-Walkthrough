5a8669f941cb851e0e6d6822783608a5da229c42
Matteo Giuntoni <59317264+Matteo181202@users.noreply.github.com>
2023-08-17 18:36:41 +0200

    Fix color

--- framework/Texture.cpp
+++ framework/Texture.cpp
@@ -12,7 +12,7 @@ Texture::Texture(const std::string &path) : rendererID(0),
 {
     stbi_set_flip_vertically_on_load(true);
     this->localBuffer = stbi_load(this->filePath.c_str(), &this->width, &this->height, &this->BPP, 4);
-    GLenum format = GL_RED;
+    GLenum format;
     if (this->BPP == 1)      format = GL_RED;
     else if (this->BPP == 3) format = GL_RGB;
     else if (this->BPP == 4) format = GL_RGBA;
@@ -20,13 +20,13 @@ Texture::Texture(const std::string &path) : rendererID(0),
 
     glGenTextures(1, &this->rendererID);
     glBindTexture(GL_TEXTURE_2D, this->rendererID);
-    glTexImage2D(GL_TEXTURE_2D, 0, format, this->width, this->height, 0, format, GL_UNSIGNED_BYTE, this->localBuffer);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, this->width, this->height, 0, GL_RGBA, GL_UNSIGNED_BYTE, this->localBuffer);
     glGenerateMipmap(GL_TEXTURE_2D);
 
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, format == GL_RGBA ? GL_CLAMP_TO_EDGE : GL_REPEAT); // for this tutorial: use GL_CLAMP_TO_EDGE to prevent semi-transparent borders. Due to interpolation it takes texels from next repeat
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, format == GL_RGBA ? GL_CLAMP_TO_EDGE : GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
 
     if(this->localBuffer) stbi_image_free(this->localBuffer);
 }
