976f2ff5c0f002d5f4ada2774ee5534239744d5a
Matteo Giuntoni <59317264+Matteo181202@users.noreply.github.com>
2023-09-25 23:04:24 +0200

    Add blending

--- Main.cpp
+++ Main.cpp
@@ -61,6 +61,8 @@ int main(void){
     glewInit();
 
     glEnable(GL_DEPTH_TEST);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     printout_opengl_glsl_info();
 
     float skyboxVertices[] = {
